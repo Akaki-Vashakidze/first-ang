5a501e324d828b5a1910db5a9f1fc034429f76fc
Akaki <Akaki@Pro1.local>
2022-09-27 11:12:42 +0400

    fff

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button'
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 
 const MaterialComponents = [
   MatMenuModule,
-  MatButtonModule
+  MatButtonModule,
+  MatToolbarModule
 ]
 
 @NgModule({
